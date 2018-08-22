f1b9d04701785b11eced0c3ddaf6ffc82a14e906
Jayasagar <jayasagar@gmail.com>
2018-08-22 10:50:57 +0200

    DAY 22 ASO-2018 Create 3,3,2 shape array to simulate sigmoid as a perceptron

--- SigmoidFestivalDecision.py
+++ SigmoidFestivalDecision.py
@@ -16,8 +16,8 @@ Leave  = 6
 Bias : If All have equal priority, then bias will be less
 '''
 
-inputs = [1, 0, 1] # inputs[weather, train, leave]
-weights = [2, 2, 6] # weights[weather, train, leave]
+inputs = [0, 0, 1] # inputs[weather, train, leave]
+weights = [2, 3, 5] # weights[weather, train, leave]
 
 def sigmoid(inputs, weights, bias):
     productAndSumOfInputsWeight = (inputs * weights).sum()
@@ -29,4 +29,30 @@ def sigmoid(inputs, weights, bias):
     return sigmoid
 
 
-print('Am I going to Festival:', sigmoid(np.array(inputs), np.array(weights), 5))
+# print('Am I going to Festival:', sigmoid(np.array(inputs), np.array(weights), 5))
+
+
+# Excercise 1: Sigmoid neurons simulating perceptrons
+'''
+Suppose we take all the weights and biases in a network of perceptrons, and multiply them by a positive constant, c>0. 
+Show that the behaviour of the network doesn't change
+'''
+def simulateSigmoidAsPerceptron(inputs, weights, bias):
+    # Weight should positive number > 0
+    productAndSumOfInputsWeight = (inputs * weights).sum()
+    print ('Product Array:', productAndSumOfInputsWeight)
+
+    z = productAndSumOfInputsWeight + (-bias)
+
+    # Sigmoid value should not impact the netwowrk as it alwys greater than 1.
+    sigmoid = 1 / 1 + math.exp(-z)
+    return sigmoid
+
+sigmoidSimulationInput = np.array([
+    [[0, 2], [0, 3], [1, 5]],
+    [[1, 3], [1, 3], [0, 5]],
+    [[0, 4], [1, 2], [1, 4]]
+])
+
+print ('sigmoidSimulationInput Array Shape:', sigmoidSimulationInput.shape)
+# print('simulateSigmoidAsPerceptron:', simulateSigmoidAsPerceptron(np.array(inputs), np.array(weights), 4))
