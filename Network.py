e1e64fa41f569cfb2334ff9fe4572eb2028cd654
Jayasagar <jayasagar@gmail.com>
2018-08-24 04:53:53 +0200

    DAY 24 ASO-2018 main class skeleton with algorithm Stochastic Gradient Descent

--- Network.py
+++ Network.py
@@ -2,8 +2,14 @@ import numpy as np
 
 class Network(object):
 
+    # the list sizes contains the number of neurons in the respective layers.
     def __init__(self, sizes):
         self.num_layers =  len(sizes)
         self.sizes = sizes
+        '''
+        The biases and weights in the Network object are all initialized randomly, 
+        using the Numpy np.random.randn function to generate Gaussian distributions with mean 0 and standard deviation 1.
+        This random initialization gives our stochastic gradient descent algorithm a place to start from
+        '''
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
---
+++ StochasticGradientDescentNN.py
@@ -0,0 +1,7 @@
+import Network as Network
+
+def main():
+    print('Hello')
+
+if __name__ == '__main__':
+    main()
